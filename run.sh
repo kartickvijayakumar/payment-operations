e2fdd50247c543c550ab1cf541de22e10987e3fc
Kartick Vijayakumar <kartickv@hypto.in>
2021-12-23 17:39:52 +0530

    Add instrument config in run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ bundle install
 
 rails generate hws:payment_operations_demo:install
 bundle exec rake db:migrate
+rake hws-instruments:hypto:payouts["e87a7c07-55cf-4e69-bf14-10006657f3fb"]
+rake hws-instruments:hypto:virtual_account["e87a7c07-55cf-4e69-bf14-10006657f3fb"]
 
 rm -f tmp/pids/server.pid
 
